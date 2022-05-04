b235967d6d40f63a9c2b4653bfaafe28f8d0b133
martinideniam <martinideniam@gmail.com>
2022-05-04 21:37:54 +0300

    Initial Commit

---
+++ set-game/set_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  set_gameApp.swift
+//  set-game
+//
+//  Created by Vladislav Gorovenko on 04.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct set_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
